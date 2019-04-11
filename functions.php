5854c36122d43549d4b38a22d88c780a10c94e29
Alessandro Tilli <alessandro@lattecreative.com>
2019-04-11 15:32:24 +0200

    partial update functions.php with some test

--- functions.php
+++ functions.php
@@ -11,3 +11,148 @@ function enqueue_child_styles() {
 
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.min.css', [], $css_creation );
 }
+
+
+/**
+ * Taxonomy show_on filter
+ * @author Bill Erickson
+ * @link https://github.com/CMB2/CMB2/wiki/Adding-your-own-show_on-filters
+ *
+ * @param bool $display
+ * @param array $metabox
+ * @return bool display metabox
+ */
+function be_taxonomy_show_on_filter( $display, $meta_box ) {
+	if ( ! isset( $meta_box['show_on']['key'], $meta_box['show_on']['value'] ) ) {
+		return $display;
+	}
+
+	if ( 'taxonomy' !== $meta_box['show_on']['key'] ) {
+		return $display;
+	}
+
+	$post_id = 0;
+
+	// If we're showing it based on ID, get the current ID
+	if ( isset( $_GET['post'] ) ) {
+		$post_id = $_GET['post'];
+	} elseif ( isset( $_POST['post_ID'] ) ) {
+		$post_id = $_POST['post_ID'];
+	}
+
+	if ( ! $post_id ) {
+		return $display;
+	}
+
+	foreach( (array) $meta_box['show_on']['value'] as $taxonomy => $slugs ) {
+		if ( ! is_array( $slugs ) ) {
+			$slugs = array( $slugs );
+		}
+
+		$display = false;
+		$terms = wp_get_object_terms( $post_id, $taxonomy );
+		foreach( $terms as $term ) {
+			if ( in_array( $term->slug, $slugs ) ) {
+				$display = true;
+				break;
+			}
+		}
+
+		if ( $display ) {
+			break;
+		}
+	}
+
+	return $display;
+}
+add_filter( 'cmb2_show_on', 'be_taxonomy_show_on_filter', 10, 2 );
+
+function register_sidebar_metabox_child() {
+
+	$prefix = 'p4_';
+
+	$p4_sidebar = new_cmb2_box(
+		[
+			'id'           => $prefix . 'metabox_sidebar',
+			'title'        => __( 'Post extra attributes', 'planet4-master-theme-backend' ),
+			'object_types' => [ 'post' ], // Post type.
+			// 'show_on' => array( 
+			// 	'key' => 'taxonomy', 
+			// 	'value' => array( 
+			// 		'p4-page-type' => array( 'update' ) 
+			// 	) 
+			// ),
+			'context'		=> 'side',
+			'priority'		=> 'low'
+		]
+	);
+	
+	// funzione per recuperare posts e usare in backend con cmb2
+	// da ottimizzare in secondo momento
+	function generate_post_select($post_type) {
+		$post_type_object = get_post_type_object($post_type);
+		$label = $post_type_object->label;
+		$posts = get_posts(array('post_type'=> $post_type, 'post_status'=> 'publish', 'suppress_filters' => false, 'posts_per_page'=>-1,'tax_query'=>array(array('taxonomy' => 'p4_post_attribute','field' => 'slug', 'terms' => 'project'))));
+		$output = array();        
+		foreach ($posts as $post) {
+			$postid = $post->ID;
+			$output[$postid] = $post->post_title;
+		}
+		return $output;
+	}
+
+	$p4_sidebar->add_field( array(
+		'name'             => esc_html__( 'Project related', 'cmb2' ),
+		'desc'             => esc_html__( 'Select a project connected to this post (optional)', 'cmb2' ),
+		'id'               => $prefix . 'select_project_related',
+		'type'             => 'select',
+		'show_option_none' => true,
+		'options'          => generate_post_select('post')
+	) );
+
+}
+
+
+add_action( 'cmb2_admin_init', 'register_sidebar_metabox_child' );
+
+
+// add_action( 'wp_footer', function () {
+//     if ( ! current_user_can( 'manage_options' ) ) {
+//         return;
+// 	}
+// 	wp_cache_add( 'sticaz', 'sti gran ca', 'test:sticaztest', 600 );
+//     $GLOBALS['wp_object_cache']->stats();
+// } );
+
+
+
+///////  custom taxonomy, andrà tolta da qui, in attesa di decidere come strutturare functions child
+// poi da vedere se riutilizziamo la loro funzione
+
+add_action('init', 'p4_child_register_taxonomy');
+function p4_child_register_taxonomy() {
+
+	$labels = array(
+		'name'              => _x( 'Special attributes', 'taxonomy general name', 'p4' ),
+		'singular_name'     => _x( 'Special attribute', 'taxonomy singular name', 'p4' ),
+		'search_items'      => __( 'Search attributes', 'p4' ),
+		'all_items'         => __( 'All attributes', 'p4' ),
+		'edit_item'         => __( 'Edit attribute', 'p4' ),
+		'update_item'       => __( 'Update attribute', 'p4' ),
+		'add_new_item'      => __( 'Add New attribute', 'p4' ),
+		'new_item_name'     => __( 'New attribute Name', 'p4' ),
+		'menu_name'         => __( 'Attribute', 'p4' ),
+	);
+
+	$args = array(
+		'hierarchical'      => true,
+		'labels'            => $labels,
+		'show_ui'           => true,
+		'show_admin_column' => true,
+		'query_var'         => true,
+		'rewrite'           => array( 'slug' => 'attribute' ),
+	);
+
+	register_taxonomy( 'p4_post_attribute', array( 'post', 'page' ), $args );
+}
+
