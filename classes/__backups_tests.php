867af685ea2d902900a4dac48bc8798f84041db9
nicklem <nicola.klemenc@gmail.com>
2019-05-24 15:14:54 +0200

    Moves commented code to classes/__backups_tests.php

---
+++ classes/__backups_tests.php
@@ -0,0 +1,7 @@
+// add_action( 'wp_footer', function () {
+//     if ( ! current_user_can( 'manage_options' ) ) {
+//         return;
+// 	}
+// 	wp_cache_add( 'sticaz', 'sti gran ca', 'test:sticaztest', 600 );
+//     $GLOBALS['wp_object_cache']->stats();
+// } );
