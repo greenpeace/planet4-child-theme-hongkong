84c8ca82f128d1c2c2cb6ad0e8715a3553729847
nicklem <nicola.klemenc@gmail.com>
2019-05-31 12:31:25 +0200

    Adds User Story custom post type

--- classes/class-p4ct-custom-post-type-register.php
+++ classes/class-p4ct-custom-post-type-register.php
@@ -30,6 +30,7 @@ class P4CT_Custom_Post_Type_Register {
 	public function register_p4_custom_post_type() {
 		$this->register_post_type_tips();
 		$this->register_post_type_team();
+		$this->register_post_type_stories();
 	}
 
 	/**
@@ -99,8 +100,8 @@ class P4CT_Custom_Post_Type_Register {
 			'all_items'          => __( 'All Team Members', 'gpea_theme' ),
 			'search_items'       => __( 'Search Team Members', 'gpea_theme' ),
 			'parent_item_colon'  => __( 'Parent Team Members:', 'gpea_theme' ),
-			'not_found'          => __( 'No team members found.', 'gpea_theme' ),
-			'not_found_in_trash' => __( 'No team members found in Trash.', 'gpea_theme' ),
+			'not_found'          => __( 'No Team Members found.', 'gpea_theme' ),
+			'not_found_in_trash' => __( 'No Team Members found in Trash.', 'gpea_theme' ),
 		);
 
 		$args = array(
@@ -122,6 +123,53 @@ class P4CT_Custom_Post_Type_Register {
 		);
 
 		register_post_type( 'team', $args );
+
+	}
+
+	/**
+	 * Register a user_story post type.
+	 *
+	 * @link http://codex.wordpress.org/Function_Reference/register_post_type
+	 */
+	public function register_post_type_stories() {
+
+		$labels = array(
+			'name'               => _x( 'User Stories', 'post type general name', 'gpea_theme' ),
+			'singular_name'      => _x( 'User Story', 'post type singular name', 'gpea_theme' ),
+			'menu_name'          => _x( 'User Stories', 'admin menu', 'gpea_theme' ),
+			'name_admin_bar'     => _x( 'User Story', 'add new on admin bar', 'gpea_theme' ),
+			'add_new'            => _x( 'Add New', 'tip', 'gpea_theme' ),
+			'add_new_item'       => __( 'Add New User Story', 'gpea_theme' ),
+			'new_item'           => __( 'New User Story', 'gpea_theme' ),
+			'edit_item'          => __( 'Edit User Story', 'gpea_theme' ),
+			'view_item'          => __( 'View User Story', 'gpea_theme' ),
+			'all_items'          => __( 'All User Stories', 'gpea_theme' ),
+			'search_items'       => __( 'Search User Stories', 'gpea_theme' ),
+			'parent_item_colon'  => __( 'Parent User Stories:', 'gpea_theme' ),
+			'not_found'          => __( 'No User Stories found.', 'gpea_theme' ),
+			'not_found_in_trash' => __( 'No User Stories found in Trash.', 'gpea_theme' ),
+		);
+
+		$args = array(
+			'labels'              => $labels,
+			'description'         => __( 'Description.', 'gpea_theme' ),
+			'exclude_from_search' => true,
+			'publicly_queryable'  => false,
+			'show_ui'             => true,
+			'show_in_nav_menus'   => false,
+			'rewrite'             => array(
+				'slug' => 'user_story',
+			),
+			'capability_type'     => 'post',
+			'taxonomies'          => array( 'category', 'post_tag' ),
+			'has_archive'         => false,
+			'hierarchical'        => false,
+			'menu_position'       => null,
+			'menu_icon'           => 'dashicons-admin-comments',
+			'supports'            => array( 'title', 'editor', 'author', 'thumbnail' ),
+		);
+
+		register_post_type( 'user_story', $args );
 
 	}
 }
