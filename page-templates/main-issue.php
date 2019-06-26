4ec5d447f6e88f58d64ef738e72cde3496afdd36
Alessandro Tilli <alessandro@lattecreative.com>
2019-06-26 18:08:34 +0200

    Main issues related and strings update

--- page-templates/main-issue.php
+++ page-templates/main-issue.php
@@ -21,6 +21,7 @@ use Timber\Timber;
 
 $context        = Timber::get_context();
 $post           = new P4_Post();
+$gpea_extra     = new P4CT_Site();
 $page_meta_data = get_post_meta( $post->ID );
 
 // Set Navigation Issues links.
@@ -40,15 +41,12 @@ if ( is_array( $page_tags ) && $page_tags ) {
 	$context['campaigns'] = $tags;
 }
 
-$issues = get_category_by_slug( 'issues' );
-$issues = $issues->term_id;
-$categories = get_the_category( $post->ID );
-$categories = array_filter( $categories , function( $cat ) use ( $issues ) {
-    return $cat->category_parent === $issues;
-});
-$categories = array_reduce( $categories, function( $acc, $cat ) {
-    return $acc . ( $cat->slug ) . ' ';
-}, '');
+$main_issues = $gpea_extra->gpea_get_main_issue( $post->ID );
+if ( $main_issues ) {
+	$main_issue_slug = $main_issues->slug;
+	$main_issue_name = $main_issues->name;
+	$main_issue_id   = $main_issues->term_id;
+}
 
 if ( has_post_thumbnail( $post->ID ) ) {
     $img_id = get_post_thumbnail_id( $post->ID );
@@ -64,6 +62,9 @@ $context['header_button_title']         = $page_meta_data['p4_button_title'][0] 
 $context['header_button_link']          = $page_meta_data['p4_button_link'][0] ?? '';
 // $context['header_button_link_checkbox'] = $page_meta_data['p4_button_link_checkbox'];
 $context['background_image']            = wp_get_attachment_url( get_post_meta( get_the_ID(), 'background_image_id', 1 ) );
-$context['custom_body_classes']         = $categories;
+$context['custom_body_classes']         = $main_issue_slug;
+$context['main_issue_id']               = $main_issue_id;
+
+$context['related_posts']               = $gpea_extra->gpea_get_related( $post->ID, 3 );
 
 Timber::render( [ 'main-issue.twig' ], $context );
