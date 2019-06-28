ada8e9d011a705f3d4a12fed3eba52608099f4bf
Nicola Klemenc <nicola.klemenc@gmail.com>
2019-06-28 13:35:47 +0200

    Search: Reactivate ep_integrate.

--- classes/class-p4ct-elasticsearch.php
+++ classes/class-p4ct-elasticsearch.php
@@ -19,7 +19,6 @@ if ( ! class_exists( 'P4CT_ElasticSearch' ) ) {
 		 */
 		public function set_engines_args( &$args ) {
 
-			// TODO IMPORTANT!! find out why setting ep_integrate = true leads to NO ajax results.
 			 $args['ep_integrate'] = true;
 			// Get only DOCUMENT_TYPES from the attachments.
 			if ( ! $this->search_query && ! $this->filters ) {
