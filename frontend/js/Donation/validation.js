eaca1d9fe28bf37b0c180b03ef3bf31146348330
tillilatte <alessandro.tilli@lattecreative.com>
2019-07-09 18:31:58 +0200

    following fix and donation

--- frontend/js/Donation/validation.js
+++ frontend/js/Donation/validation.js
@@ -43,25 +43,22 @@ const dataConstraints = {
 	length: {is: 8},
     // the validation should happen with the masking, so no other rule required here
   },
-  ['en__field--country']: {
-    presence: required,
-    // inclusion: {
-      // within: ['HK', 'TW', 'KR'],
-      // message: countryMessage,
-    // },
-  },
-  ['en__field--region']: {
-    presence: required,
-  },
-  ['en__field--city']: {
-    presence: required,
-  },
+  // ['en__field--country']: {
+    // presence: required,
+    // // inclusion: {
+      // // within: ['HK', 'TW', 'KR'],
+      // // message: countryMessage,
+    // // },
+  // },
+  // ['en__field--region']: {
+    // presence: required,
+  // },
+  // ['en__field--city']: {
+    // presence: required,
+  // },
   ['en__field--address1']: {
     presence: required,
 	length: {maximum: 255},	
-  },
-  ['supporter.NOT_TAGGED_33']: {
-	length: {maximum: 10},	
   },
 };
 
@@ -150,7 +147,8 @@ export default function(form) {
         const feedback = block.querySelector(
           '[data-gpea-constraint-name="' + name + '"] + .invalid-feedback'
         );
-        input.classList.add('is-invalid');
+		console.log(name);
+        if (input) input.classList.add('is-invalid');
         feedback.textContent = message;
       }
     }
--- frontend/js/components/followUnfollow/makechange-follower.js
+++ frontend/js/components/followUnfollow/makechange-follower.js
@@ -51,85 +51,89 @@ const makechangeFollower = function() {
     .map(el => ({
       ID: el.dataset.postid,
     }));
+	
+  let gpea_topics_followed = Cookies.get('gpea_topics');
+  if (typeof gpea_topics_followed !== 'undefined' && gpea_topics_followed.length > 0) {
 
-  $featuredSwiper.addClass('is-loading');
-  $.ajax({
-    url: p4_vars.ajaxurl,
-    type: 'post',
-    data: {
-      action: 'topicsFollowing',
-      star: 'petition',
-    },
-    success: function(data) {
-      const swiper = $featuredSwiper[0].swiper;
-      let followingResults;
-      let followingResultsPosts;
-      let posts = [];
+	  $featuredSwiper.addClass('is-loading');
+	  $.ajax({
+		url: p4_vars.ajaxurl,
+		type: 'post',
+		data: {
+		  action: 'topicsFollowing',
+		  star: 'petition',
+		},
+		success: function(data) {
+		  const swiper = $featuredSwiper[0].swiper;
+		  let followingResults;
+		  let followingResultsPosts;
+		  let posts = [];
 
-      try {
-        followingResults = JSON.parse(data);        
-      } catch (error) {
-        console.error(error);
-        $featuredSwiper.removeClass('is-loading');
-        return;
-      }
+		  try {
+			followingResults = JSON.parse(data);        
+		  } catch (error) {
+			console.error(error);
+			$featuredSwiper.removeClass('is-loading');
+			return;
+		  }
 
-      followingResults.map(postsTag => {        
-        let followingResultsPosts = postsTag.posts;
-        posts = posts.concat(followingResultsPosts);
-      });
+		  followingResults.map(postsTag => {        
+			let followingResultsPosts = postsTag.posts;
+			posts = posts.concat(followingResultsPosts);
+		  });
 
-      // Remove existing posts from the returned posts, take first 5, sort by recent first
-      // console.log(posts.length + ' posts returned');
-      posts = filterDuplicates(posts, existingPosts);
-      sortByRecentFirst(posts);
-      posts = posts.slice(0, 5);      
-      // console.log(existingPosts.length + ' existing posts');
-      // console.log(posts.length + ' posts returned');
+		  // Remove existing posts from the returned posts, take first 5, sort by recent first
+		  // console.log(posts.length + ' posts returned');
+		  posts = filterDuplicates(posts, existingPosts);
+		  sortByRecentFirst(posts);
+		  posts = posts.slice(0, 5);      
+		  // console.log(existingPosts.length + ' existing posts');
+		  // console.log(posts.length + ' posts returned');
 
-      // Create the HTML element for each new post
+		  // Create the HTML element for each new post
 
-      const petitionTemplate = $('#template-card-petition-big');
-      const buildPetition = template(petitionTemplate[0].innerHTML);
-      const updateTemplate = $('#template-card-update-big');
-      const buildUpdate = template(updateTemplate[0].innerHTML);
+		  const petitionTemplate = $('#template-card-petition-big');
+		  const buildPetition = template(petitionTemplate[0].innerHTML);
+		  const updateTemplate = $('#template-card-update-big');
+		  const buildUpdate = template(updateTemplate[0].innerHTML);
 
-      const newPostsSlides = posts.map(post => {
-        if (post.engaging_pageid !== undefined) {
-          return buildPetition(post);
-        } else {
-          return buildUpdate(post);
-        }
-      });
+		  const newPostsSlides = posts.map(post => {
+			if (post.engaging_pageid !== undefined) {
+			  return buildPetition(post);
+			} else {
+			  return buildUpdate(post);
+			}
+		  });
 
-      // Add the new slides in 6th position (1 first slide + 5 regular slides)
-      swiper.addSlide(6, newPostsSlides);
-      // console.log(
-      //   newPostsSlides.length + ' slides created and added',
-      //   newPostsSlides
-      // );
+		  // Add the new slides in 6th position (1 first slide + 5 regular slides)
+		  swiper.addSlide(6, newPostsSlides);
+		  // console.log(
+		  //   newPostsSlides.length + ' slides created and added',
+		  //   newPostsSlides
+		  // );
 
-      // Remove slides beyond 11 (1 first slide + 10 regular slides)
-      const $slides = $featuredSwiper.find('.swiper-slide');
-      // console.log($slides.length + ' new number of total slides');
-      const totalSlides = $slides.length;
-      if (totalSlides > 11) {
-        const slidesToRemove = [];
-        for (let i = 11; i < totalSlides; i++) {
-          slidesToRemove.push(i);
-        }
-        swiper.removeSlide(slidesToRemove);
-        // console.log(slidesToRemove, 'slides removed');
-      }
+		  // Remove slides beyond 11 (1 first slide + 10 regular slides)
+		  const $slides = $featuredSwiper.find('.swiper-slide');
+		  // console.log($slides.length + ' new number of total slides');
+		  const totalSlides = $slides.length;
+		  if (totalSlides > 11) {
+			const slidesToRemove = [];
+			for (let i = 11; i < totalSlides; i++) {
+			  slidesToRemove.push(i);
+			}
+			swiper.removeSlide(slidesToRemove);
+			// console.log(slidesToRemove, 'slides removed');
+		  }
 
-      // All done
-      $featuredSwiper.removeClass('is-loading');
-    },
-    error: function(errorThrown) {
-      $featuredSwiper.removeClass('is-loading');
-      console.error(errorThrown);
-    },
-  });  
+		  // All done
+		  $featuredSwiper.removeClass('is-loading');
+		},
+		error: function(errorThrown) {
+		  $featuredSwiper.removeClass('is-loading');
+		  console.error(errorThrown);
+		},
+	  });  
+	}
 };
 
 export default makechangeFollower;
