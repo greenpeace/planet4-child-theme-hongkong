8caefab342f21c21ef77a98a2d9ae22bf5ffe579
Manuele Sarfatti <mjsarfatti@gmail.com>
2019-09-08 19:46:20 +0200

    dynamic search in all pages

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -29,6 +29,7 @@ import parallax from './components/parallax';
 import scrollWay from './components/scroll-way';
 import articleList from './components/article-list';
 import facebookVideo from './components/facebook-video';
+import dynamicSearch from './components/dynamic-search';
 
 import petitionThankyou from './petition-thankyou';
 
@@ -83,6 +84,8 @@ document.addEventListener('DOMContentLoaded', function(event) {
   articleList();
 
   facebookVideo();
+
+  dynamicSearch();
 
   AOS.init({
     easing: 'ease-in-out-cubic',
