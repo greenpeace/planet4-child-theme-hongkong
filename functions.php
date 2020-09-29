cfbc392fb353b2f22efb710a9f548ed418f9a5b1
Jason <>
2020-09-29 10:09:09 +0800

    update the html tags for SEO

--- functions.php
+++ functions.php
@@ -16,5 +16,34 @@ $services = [
 	'P4CT_Shortcode',
 ];
 
+
+
 new P4CT_Site( $services );
+
+add_filter( 'img_caption_shortcode', 'my_img_caption_shortcode', 10, 3 );
+
+function my_img_caption_shortcode( $empty, $attr, $content ){
+	$attr = shortcode_atts( array(
+		'id'      => '',
+		'align'   => 'alignnone',
+		'width'   => '',
+		'caption' => ''
+	), $attr );
+
+	if ( 1 > (int) $attr['width'] || empty( $attr['caption'] ) ) {
+		return '';
+	}
+
+	if ( $attr['id'] ) {
+		$attr['id'] = 'id="' . esc_attr( $attr['id'] ) . '" ';
+	}
+
+	return '<figure ' . $attr['id']
+	. 'class="wp-caption ' . esc_attr( $attr['align'] ) . '" '
+	. 'style="max-width: ' . ( 10 + (int) $attr['width'] ) . 'px;">'
+	. do_shortcode( $content )
+	. '<figcaption class="wp-caption-text">' . $attr['caption'] . '</figcaption>'
+	. '</figure>';
+
+}
 
