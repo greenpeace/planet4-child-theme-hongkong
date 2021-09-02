3755f1ee141bf00de2977fff607de6f6b9f5c1e9
hit1205 <hit1205.tw@gmail.com>
2021-09-02 16:20:33 +0800

    Revert "Reverted the script to add className to .section-projects"
    This reverts commit 4a7118fb29709b9fca458e206fee90631a19aa50.

--- frontend/js/components/follow-unfollow.js
+++ frontend/js/components/follow-unfollow.js
@@ -1,13 +1,11 @@
 import pageProject from './followUnfollow/page-project';
 import pageIssue from './followUnfollow/page-issue';
 import tagCloud from './followUnfollow/tag-cloud';
-import homeFollower from './followUnfollow/home-follower';
 
 const followUnfollow = function() {
   pageProject();
   pageIssue();
   tagCloud();
-  homeFollower();
 };
 
 export default followUnfollow;
