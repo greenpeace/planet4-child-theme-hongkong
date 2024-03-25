48012949a6b8b1b0c8bb0fd1771e8919391bbed5
hit1205 <hit1205.tw@gmail.com>
2024-03-25 12:46:43 +0800

    Removed live search on header

--- frontend/js/search-page.js
+++ frontend/js/search-page.js
@@ -1,4 +1,3 @@
-import template from 'lodash.template';
 import './vendor/jquery.auto-complete.min.js';
 
 $ = jQuery; // eslint-disable-line no-global-assign
@@ -7,61 +6,11 @@ const p4ct_search = function() {
   const $search_form = $('#search_form_inner');
   if (!$search_form.length){ return; }
 
-  var search_request, live_search_timer, last_search_query;
+  var search_request;
   var search_query = $('#search_input').val().trim();
-  const ajaxurl = window.localizations.ajaxurl; // eslint-disable-line no-undef
 
-  const $live_result_posts = $('#ajax-search-posts');
-
-  const $post_template = $('#template-posts');
-  const build_posts = template($post_template[0].innerHTML);
-
-  const is_result_page = $('body.search').length > 0;
   const $result_page_result_posts = $('.multiple-search-result .results-list');
   const $load_more_button = $('.btn-load-more-click-scroll');
-
-  if(!is_result_page) {
-    $('.search-autocomplete').autoComplete({
-      minChars: 2,
-      source: function(term, suggest) {
-        try {
-          search_request.abort();
-        } catch (e) {} // eslint-disable-line no-empty
-
-        $search_form.addClass('is-loading');
-        $live_result_posts.addClass('fade-out');
-
-        search_request = $.post(
-          ajaxurl,
-          {
-            action: 'p4ct_search_site',
-            search: term,
-          },
-          function(res) {
-            $('.nothing-found').remove();
-            $search_form.removeClass('is-loading');
-            // Frontend TODO: integrate
-            res = JSON.parse(res);
-            // window.res = res;
-            // console.log(res);
-            const posts = res.posts;
-
-            if (posts.length) { $live_result_posts.show().removeClass('fade-out'); }
-            else { $live_result_posts.hide(); }
-
-            // posts = posts.map(post => post.post_title);
-            // posts = posts.join('<br>');
-            // posts = posts || 'No posts found';
-            $live_result_posts.find('.results-list')[0].innerHTML = build_posts({
-              posts: posts,
-            });
-            // console.log(posts);
-            // suggest(res.data);
-          }
-        );
-      },
-    });
-  }
 
   $search_form.on('submit', function(e) {
     $(document.body).addClass('is-loading');
@@ -93,7 +42,6 @@ const p4ct_search = function() {
     try {
       search_request.abort();
     } catch (e) {}
-    const current_params = new URLSearchParams(location.search);
     search_request = $.ajax({
       url: window.localizations.ajaxurl,
       type: 'GET',
